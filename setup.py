ddf765b4f378a16514839df583a6bd8f75cc8c2b
Julia-Kettle <s1035546@sms.ed.ac.uk>
2016-06-02 09:55:41 +0100

    updated setup + tidied main

--- setup.py
+++ setup.py
@@ -1,72 +1,135 @@
 import numpy as np
 
 class Params:
-    '''
-    defines parameters depending on lattice type. 
-    taken from Nicholas matlab code
-    Need to figure out how to return these values easily...
-    Maybe put into main code??
-    '''
-    def __init__(self, lattice):
-        self.latticename = lattice
+    	'''
+    	defines parameters depending on lattice type. 
+    	taken from Nicholas matlab code
+    	Need to figure out how to return these values easily...
+    	Maybe put into main code??    
+	'''
+	def __init__(self, beta):
 
-        if lattice == '24':
-            self.m_sea_s = 0.04
-            self.m_sea_s_phys = 0.03224
-            self.m_sea_s_pq = 0.03
-            self.m_sea_l = [0.0050, 0.0100, 0.0200]
-            self.m_val_s = [0.0300, 0.0350, 0.0400]
-            self.n_l_m = 1 
-            self.n_uni_pi = 2
-            self.kaon_f = 2
-            self.kaon_l = 4
-            self.n_m_val_l = 1
-            self.n_m_val_s = len(self.m_val_s)
-            self.n_m_sea_l = len(self.m_sea_l)
-            self.norm = pow(24,3)
-            self.twall = 1
-            self.cps_conv = 1 ##???
-            self.n_chan = 5;
+        	if beta == 0:
+			self.latticename = '24'
+            		self.ainv = 1.7845
+			self.m_sea_s = 0.04
+            		self.m_sea_s_phys = 0.03224
+            		self.m_sea_s_pq = 0.03
+            		self.m_sea_l = [0.0050, 0.0100, 0.0200]
+            		self.m_val_s = [0.0300, 0.0350, 0.0400]
+            		self.n_l_m = 1 
+            		self.n_uni_pi = 2
+            		self.kaon_f = 2
+            		self.kaon_l = 4
+            		self.n_m_val_l = 1
+            		self.n_m_val_s = len(self.m_val_s)
+            		self.n_m_sea_l = len(self.m_sea_l)
+            		self.norm = pow(24,3)
+            		self.twall = 1
+            		self.cps_conv = 1 ##???
+            		self.n_chan = 5;
         
-        elif lattice == '32':
-            self.m_sea_s = 0.04
-            self.m_sea_s_phys = 0.02477
-            self.m_sea_s_pq = 0.025
-            self.m_sea_l = [0.0040, 0.0060, 0.0080]
-            self.m_val_s = [0.0250, 0.0300]
-            self.n_l_m = 10
-            self.n_uni_pi = 3
-            self.kaon_f = 11
-            self.kaon_l = 19
-            self.n_m_val_l = 1
-            self.m_m_val_s = len(self.m_val_s)
-            self.n_m_sea_l = len(self.m_sea_l)
-            self.norm = pow(32,3)
-            self.twall = 1
-            self.cps_conv = 3.2 ##???
-            self.n_chan = 5;
+        	elif beta == 1:
+			self.latticename = '32'
+			self.ainv = 2.3883
+            		self.m_sea_s = 0.04
+            		self.m_sea_s_phys = 0.02477
+            		self.m_sea_s_pq = 0.025
+            		self.m_sea_l = [0.0040, 0.0060, 0.0080]
+            		self.m_val_s = [0.0250, 0.0300]
+            		self.n_l_m = 10
+            		self.n_uni_pi = 3
+            		self.kaon_f = 11
+           	 	self.kaon_l = 19
+            		self.n_m_val_l = 1
+            		self.m_m_val_s = len(self.m_val_s)
+            		self.n_m_sea_l = len(self.m_sea_l)
+            		self.norm = pow(32,3)
+            		self.twall = 1
+            		self.cps_conv = 3.2 ##???
+            		self.n_chan = 5;
 
-        self.name_chan= ['bPS_bPS','bPS_bA0','bPS_pPS','bPS_pA0','bA0_pA0']
-        self.sgn_chan = [1,-1,1,-1,1]
-        self.n_src = 2;
-        self.name_src = ['sr','sk']
 
-    def show(self,values):
-        variables = self.__dict__
-        if values == "all":
-            for key in variables:
-                print key + " - ", variables[key]
-                print key + " - ", variables[key]
-        if type(values) is list:
-            for i in values:
-                print i + " - ", variables[i]
-                print i + " - ", variables[i]
-        elif type(values) is str:
-            print values + " - ", variables[values]
-            print values + " - ", variables[values]
-        else:
-            print 'Error, wrong data types passes, must pass show "all", a string or a list of strings' 
-            print 'Error, wrong data types passes, must pass show "all", a string or a list of strings' 
+        	elif beta == 1:
+			self.latticename = '48'
+			self.ainv = 2.3883
+            		self.m_sea_s = 0.0362
+            		self.m_sea_s_phys = 0.02477
+            		self.m_sea_s_pq = 0.025
+            		self.m_sea_l = [0.00078]
+            		self.m_val_s = [0.0362]
+            		self.n_l_m = 10
+            		self.n_uni_pi = 1
+            		self.kaon_f = 11
+           	 	self.kaon_l = 19
+            		self.n_m_val_l = 1
+            		self.m_m_val_s = len(self.m_val_s)
+            		self.n_m_sea_l = len(self.m_sea_l)
+            		self.norm = pow(32,3)
+            		self.twall = 1
+            		self.cps_conv = 3.2 ##???
+            		self.n_chan = 5;
+        	
+		elif beta == 3:
+			self.latticename = '48fine'
+			self.ainv = 2.3883
+            		self.m_sea_s = 0.02661
+            		self.m_sea_s_phys = 0.02661
+            		self.m_sea_s_pq = 0.02661
+            		self.m_sea_l = [0.002661]
+            		self.m_val_s = [0.02661]
+            		self.n_l_m = 10
+            		self.n_uni_pi = 3
+            		self.kaon_f = 11
+           	 	self.kaon_l = 19
+            		self.n_m_val_l = 1
+            		self.m_m_val_s = len(self.m_val_s)
+            		self.n_m_sea_l = len(self.m_sea_l)
+            		self.norm = pow(32,3)
+            		self.twall = 1
+            		self.cps_conv = 3.2 ##???
+            		self.n_chan = 5;
+        	
+		elif beta == 4:
+			self.latticename = '64'
+			self.ainv = 2.3883
+            		self.m_sea_s = 0.02114
+            		self.m_sea_s_phys = 0.02114
+            		self.m_sea_s_pq = 0.02114
+            		self.m_sea_l = [0.000678]
+            		self.m_val_s = [0.02114]
+            		self.n_l_m = 10
+            		self.n_uni_pi = 3
+            		self.kaon_f = 11
+           	 	self.kaon_l = 19
+            		self.n_m_val_l = 1
+            		self.m_m_val_s = len(self.m_val_s)
+            		self.n_m_sea_l = len(self.m_sea_l)
+            		self.norm = pow(32,3)
+            		self.twall = 1
+            		self.cps_conv = 3.2 ##???
+            		self.n_chan = 5;
+
+
+
+
+		self.name_chan= ['bPS_bPS','bPS_bA0','bPS_pPS','bPS_pA0','bA0_pA0']
+		self.sgn_chan = [1,-1,1,-1,1]
+       		self.n_src = 2;
+		self.name_src = ['sr','sk']
+
+	def show(self,values="all"):
+        	variables = self.__dict__
+        	if values == "all":
+            		for key in variables:
+                		print key + " - ", variables[key]
+        	elif type(values) is list:
+            		for i in values:
+                		print i + " - ", variables[i]
+        	elif type(values) is str:
+            		print values + " - ", variables[values]
+        	else:
+            		print 'Error, wrong data types passes, must pass show "all", a string or a list of strings' 
 
  
  
