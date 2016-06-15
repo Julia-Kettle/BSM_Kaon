cb591568629ca728933c1f19898c6e2a91f8c52b
Julia-Kettle <s1035546@sms.ed.ac.uk>
2016-06-15 08:46:44 +0100

    main.py updated for new correct data

--- Julia_data/cpMyData.sh
+++ Julia_data/cpMyData.sh
@@ -4,7 +4,7 @@ for lat in 48cubed 48cubedfine 64cubed
 do
 
 
-for f in /Home/s1035546/Fits/${lat}/mass/*/*.dat
+for f in /home/julia/Fits/${lat}/mass/*/*.dat
 do
 #echo $f
 name=`basename $f`
@@ -24,9 +24,9 @@ cp $f $totalpath$newname.dat
 done
 
 
-for c in 0 3 5 7 9
+for c in 0 2 4 6 8
 do
-for f in /Home/s1035546/Fits/${lat}/bag/channel${c}/*/*.dat
+for f in /home/julia/Fits/${lat}/bag/channel${c}/*/*.dat
 do
 
 name=`basename $f`
@@ -48,27 +48,5 @@ done
 done
 
 
-for c in 2 4 6 8
-do
-for f in /Home/s1035546/Fits/${lat}/bag/channel${c}/*/*.dat
-do
-
-name=`basename $f`
-dir=`dirname $f`
-
-channelname=channel$((c/2 + 1))
-mesondir=`echo ${name%%-dt*}`
-dtdir=`echo $name | cut -d "-" -f 4 | cut -d "_" -f 1`
-
-totalpath=./ratio/$lat/$mesondir/$dtdir/
-echo $name
-
-mkdir -p $totalpath
-
-cp $f $totalpath/$mesondir-$channelname.dat
-
-
-done
-done
 
 done
